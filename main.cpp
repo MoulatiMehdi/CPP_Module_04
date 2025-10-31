fcc8518114367918480612a7c3ddbeddf8c880c1
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-10-31 18:27:26 +0100

    update main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-
 #include "Animal.hpp"
 
-int main(int , char *[])
+int main(int, char *[])
 {
     Animal a;
     Animal b("Cat");
     Animal c = a;
 
     c = b;
+
+    c.makeSound();
+    b.makeSound();
+    a.makeSound();
     return 0;
 }
