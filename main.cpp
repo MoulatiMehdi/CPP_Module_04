7039658c1f6eda07339d13efee8155522f4e88f1
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-11-01 15:30:05 +0100

    add tests for WrongCat and WrongAnimal into main.cpp

--- main.cpp
+++ main.cpp
@@ -6,25 +6,26 @@
 
 int main()
 {
-    const Animal *meta = new Animal();
-    const Animal *j    = new Dog();
-    const Animal *i    = new Cat();
+    {
+        const Animal *meta = new Animal();
+        const Animal *j    = new Dog();
+        const Animal *i    = new Cat();
 
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
+        std::cout << j->getType() << " " << std::endl;
+        std::cout << i->getType() << " " << std::endl;
 
-    i->makeSound(); // will output the cat sound!
-    j->makeSound();
-    meta->makeSound();
+        i->makeSound(); // will output the cat sound!
+        j->makeSound();
+        meta->makeSound();
+    }
+    {
+        const WrongAnimal *meta = new WrongAnimal();
+        const WrongAnimal *i    = new WrongCat();
 
-    const WrongAnimal *meta = new WrongAnimal();
-    const WrongAnimal *i    = new WrongCat();
+        std::cout << i->getType() << " " << std::endl;
 
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-
-    i->makeSound(); // will output the cat sound!
-    j->makeSound();
-    meta->makeSound();
+        i->makeSound(); // will output the cat sound!
+        meta->makeSound();
+    }
     return 0;
 }
