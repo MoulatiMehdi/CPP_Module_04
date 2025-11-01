3a304131f5603f7decfa8d2772529721df7afa59
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-11-01 11:49:38 +0100

    update main.cpp file

--- main.cpp
+++ main.cpp
@@ -8,8 +8,13 @@ int main(int, char *[])
 
     c = b;
 
-    c.makeSound();
-    b.makeSound();
-    a.makeSound();
+    const Animal *const arr[] = {&a, &b, &c};
+    int                 size  = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i]->getType();
+        arr[i]->makeSound();
+    }
     return 0;
 }
