27abafc5f5fbb5cf634e50a284d81e113fd352af
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-11-01 18:50:36 +0100

    update the main and Makefile of the ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,72 @@
+#include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include <iostream>
+#include <string>
+
+void title(const std::string &name)
+{
+    const std::string rule(50, '-');
+    std::cout << rule << std::endl;
+    std::cout << name << std::endl;
+    std::cout << rule << std::endl;
+}
+
+int main()
+{
+    title("\t Virtual Destructor");
+    {
+        int     size = 6;
+        Animal *animals[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            if (i * 2 > size)
+                animals[i] = new Cat();
+            else
+                animals[i] = new Dog();
+        }
+        std::cout << std::endl;
+
+        for (int i = 0; i < size; i++)
+        {
+            delete animals[i];
+        }
+    }
+    title("\t Deep Copy");
+    {
+        Cat a;
+        a.makeSound();
+        std::cout << std::endl;
+        {
+            Cat b = a;
+            b.makeSound();
+        }
+        std::cout << std::endl;
+        Dog c;
+        c.makeSound();
+        std::cout << std::endl;
+        {
+            Dog d = c;
+            d.makeSound();
+        }
+
+        std::cout << std::endl;
+    }
+    title("\t Const Objects");
+    {
+        const Cat a;
+        Cat b = a;
+
+        b = a;
+    }
+    std::cout << std::endl;
+    {
+        const Dog a;
+        Dog b = a;
+
+        b = a;
+    }
+    std::cout << std::endl;
+    return 0;
+}
