d9613ef1107364a4aa57affb3da0961648c98a06
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-11-04 17:47:38 +0100

    tests AMateria class with its derived class

---
+++ ex03/main.cpp
@@ -0,0 +1,26 @@
+#include "AMateria.hpp"
+#include "Cure.hpp"
+#include "Ice.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    AMateria *a = new Cure();
+    AMateria *b = new Ice();
+    AMateria *c = a->clone();
+    AMateria *d = b->clone();
+
+    std::cout << a << "\n" << c << std::endl;
+    std::cout << b << "\n" << d << std::endl;
+
+    std::cout << a->getType() << std::endl;
+    std::cout << b->getType() << std::endl;
+    std::cout << c->getType() << std::endl;
+    std::cout << d->getType() << std::endl;
+
+    delete a;
+    delete b;
+    delete c; 
+    delete d;
+    return 0;
+}
