3196b8c38f84128ca7e4287501c611170c63827a
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-11-09 18:03:08 +0100

    add more test for the pool class

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,26 +1,24 @@
-#include "AMateria.hpp"
 #include "Cure.hpp"
 #include "Ice.hpp"
-#include "Pool.hpp"
 #include <iostream>
 #include <string>
 
 int main(int, char *[])
 {
     int       n = 5;
-    Pool      a;
     AMateria *b[n];
 
+    std::cout << "size of AMateria is : " << sizeof(b[0]) << std::endl;
     for (int i = 0; i < n; i++)
     {
         if (i % 2 == 0)
             b[i] = new Ice();
         else
             b[i] = new Cure();
-        a.add(*b[i]);
     }
-    a.add(*b[0]);
-    a.remove(*b[2]);
+
+    delete b[4];
+    delete b[0];
     std::cout << std::string(50, '-') << std::endl;
 
     return 0;
